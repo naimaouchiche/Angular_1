02e54330411e851e31104161b70b931c49e5127a
FIRST_AOUCHICHE LAST_NAIM <naim.aouchiche@sncf.fr>
2022-10-26 13:35:31 +0200

    form

---
+++ myFirstApp/src/app/models/user.ts
@@ -0,0 +1,22 @@
+export class User {
+    constructor(
+        public firstname: string,
+        public lastname: string,
+        public email: string,
+        public password: string
+    ) {}
+}
+
+// export class Order {
+//     title: string;
+//     quantity: number;
+//     date: Date;
+//     contact: string;
+
+//     constructor(title: string, quantity: number, date: Date, contact: string) {
+//         this.title = title;
+//         this.quantity = quantity;
+//         this.date = date
+//         this.contact = contact;
+//     }
+// }
